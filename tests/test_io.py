08365b04d977dfe07067921ffdc10efbb9d75bce
Bobby Jackson <rjackson@anl.gov>
2025-08-15 12:34:55 -0500

    FIX: More tolerance for io test

--- tests/test_io.py
+++ tests/test_io.py
@@ -9,4 +9,4 @@ def test_preprocess_radar_image():
     rad_scan = adam.io.preprocess_radar_image('KLOT', '2025-07-15T18:00:00')
     assert rad_scan.pytorch_image.shape == torch.Size([1, 3, 256, 256])
     assert rad_scan.pyart_object.fields['reflectivity']['data'].shape == (6480, 1832)
-    assert rad_scan.pytorch_image.numpy().sum() == 150341140.0
+    np.testing.assert_almost_equal(rad_scan.pytorch_image.numpy().sum(), 150341140.0, decimal=-4)
