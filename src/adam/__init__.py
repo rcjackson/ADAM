fca2364b6ebc244413ac35638328d954c3e8d29f
Bobby Jackson <rjackson@anl.gov>
2025-09-08 12:58:37 -0500

    Update __init__.py

--- src/adam/__init__.py
+++ src/adam/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Robert Jackson, Seongha Park"""
 __email__ = 'rjackson@anl.gov'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 from . import io  # noqa
 from . import model   # noqa
