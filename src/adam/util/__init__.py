0fdec6a5584988e0532bbcbd64e3b95b336c74b1
Bobby Jackson <rjackson@anl.gov>
2025-09-08 09:53:10 -0500

    ADD: Docstring

--- src/adam/util/__init__.py
+++ src/adam/util/__init__.py
@@ -1,1 +1,15 @@
+"""
+===============================================
+ADAM Utility Functions (:mod:`adam.util`)
+=============================================== 
+
+.. currentmodule:: adam.util
+
+This module contains utility functions for the ADAM package.
+.. autosummary::
+    :toctree: generated/
+
+    azimuth_point
+"""
+
 from .instrument_steering import azimuth_point
