c5317587df3f0a4082b21403302b5d9d624ed9a8
Robert Jackson <rjackson@Roberts-MacBook-Pro.local>
2026-02-09 16:24:24 -0600

    FIX: Docstring for triggering.

--- src/adam/triggering/__init__.py
+++ src/adam/triggering/__init__.py
@@ -5,9 +5,11 @@ ADAM Triggering Module
 .. currentmodule:: adam.triggering
 
 This module handles the generation of scan strategies and triggering of the lidar based on radar data.
+
 .. autosummary::
 
     :toctree: generated/
+
     make_scan_file
     send_scan
     trigger_lidar_ppis_from_mask
