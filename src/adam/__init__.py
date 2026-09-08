292e75583461bc51c35283c04078474385774168
Bobby Jackson <rjackson@anl.gov>
2026-09-08 16:17:42 -0500

    Bump version to 0.6.0

--- src/adam/__init__.py
+++ src/adam/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for ATMOS Analogue Digital Twin."""
 
 
-__version__ = '0.5.0'
+__version__ = '0.6.0'
 __author__ = """Robert Jackson, Bhupendra Raut, Seongha Park, Troy Arcomano, Scott Collis"""
 
 
